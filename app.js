1dd716925d69e599ee5bb64140a036b4ffbf2993
hogan26 <nelson.gonzalezguerra@gmail.com>
2019-10-25 17:03:53 -0300

    segundo commit

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-//cuando el nombre de los archivos aparece en verde, significa que no tienen seguimiento
+//al escribir el comando git version me entrega la version instalada de git
+//el comando git init crea el repositorio usando la carpeta del proyecto, este paso se hace solo una vez
+//cuando el nombre de los archivos aparece en verde, significa que no tienen seguimiento, otra forma de 
+//verificar esto es con el comando git status -s
+//si quiero realizar seguimiento a un archivo de mi proyecto lo agrego con el comando git add "nombre del archivo"
+
