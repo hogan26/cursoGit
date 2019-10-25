af754be710b77ff4c2bb996d0702cfbc6cd88931
hogan26 <nelson.gonzalezguerra@gmail.com>
2019-10-25 17:32:56 -0300

    quinto commit

--- app.js
+++ app.js
@@ -7,4 +7,17 @@
 //mensaje) "mensaje"
 //para agregar todos los archivos al area temporal usamos el comando git add .
 //cada vez que yo haga un cambio debo mandarlo al area temporal antes de hacer un commit
+//para listar todos los commit que se han realizado yo los puedo hacer con el comando git log --oneline
+//los commit tiene un identificador y los comentarios que se van agregando
+//para volver a un commit anterior en caso de querer revertir algun cambio, se usa el comando git reset --hard identificador_del_commit
+
+
+
+
+//luego de crear el repositorio en github usamos los comandos que nos salen ahi
+//el primero es para indicar que estaremos trabajando de forma remota
+//git remote add origin https://github.com/hogan26/cursoGit.git
+//lo cual crea un identificador del proyecto que estamos realizando con git en la url especificada, este pasos e hace solo una vez
+//luego de realizar todos los commit correspondientes usaremos el comando git push -u origin master
+//para poder subir todos los archivos a github
 
