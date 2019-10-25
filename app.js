3f763ddc22343c6359a6b1ff6c7d978b2f49ae6f
hogan26 <nelson.gonzalezguerra@gmail.com>
2019-10-25 17:07:44 -0300

    tercer commit

--- app.js
+++ app.js
@@ -3,4 +3,7 @@
 //cuando el nombre de los archivos aparece en verde, significa que no tienen seguimiento, otra forma de 
 //verificar esto es con el comando git status -s
 //si quiero realizar seguimiento a un archivo de mi proyecto lo agrego con el comando git add "nombre del archivo"
+//si queremos guardar una copia o una fotografia de ese archivo usamos en comando git commit -m (para añadir un
+//mensaje) "mensaje"
+//para agregar todos los archivos al area temporal usamos el comando git add .
 
